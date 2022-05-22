2251103209f407eb85e4112ce8d5de11b7573cd5
Christina Lara <christinaalara@gmail.com>
2022-05-21 20:52:32 -0700

    Solution to Is n divisible by x and y?

---
+++ 8kyu/is_n_divisible_by_x_and_y/main.js
@@ -0,0 +1,10 @@
+function isDivisible(n, x, y) {
+
+    // check if n is divisible by two numbers
+      if(n % x == 0 && n % y == 0){
+        return true
+      }else{
+        return false
+      }
+        
+    }
